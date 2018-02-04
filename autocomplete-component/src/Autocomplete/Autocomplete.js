9f4b2b7bd238dbef152b58102faff9a7b0eeaa01
Pawel Licznerski <pawel.licznerski3@gmail.com>
2018-02-05 00:37:47 +0100

    adding some stuff

--- autocomplete-component/src/Autocomplete/Autocomplete.js
+++ autocomplete-component/src/Autocomplete/Autocomplete.js
@@ -1,25 +1,80 @@
-import React, { Component } from 'react';
-import * as countries from './../countries.json';
+import React, { Component} from 'react';
+import  countries from './../countries.json';
 
 export default class Autocomplete extends Component{
-  // constructor(props){
-  //   super(props);
-  // }
+  constructor(props){
+    super(props);
+    this.state={
+      inputValue:"",
+      countries:countries,
+    }
+  }
+  handleInputChange =(e)=>{
+    e.preventDefault();
+    const target = e.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;//potrzebne w razie dolączenia checkbox-a
+    const name = target.name;
+    // e.pattern = `[\\w\\s+#]{${value.length}}`;
+
+    let filteredOptions = [];
+
+    for (let i = 0;i<countries.length;i++) { // this is the idea of creating div//
+        const item = countries[i];
+        console.log(item.name,value, item.name.indexOf(value));
+        if (item.name.indexOf(value) == 0) {
+            filteredOptions.push({
+                name: item.name,
+                code: item.code
+            });
+        } else if (item.code.indexOf(value) == 0) {
+            filteredOptions.push({
+                name: item.name,
+                code: item.code
+            });
+        }
+    }
+
+    this.setState({
+       [name]: value,
+       countries: filteredOptions,
+     });
+  } //end of handleInputChange
+
+  handleOnFocus(){
+    this.setState({
+       countries: countries,
+     });
+  }
+  handleOnBlur(){
+    this.setState({
+       countries: countries,
+       inputValue:''
+     });
+   }
+ //  document.querySelector("#inputValue")
+ // .oninput = function() {
+ //   this.pattern = `[//w//s+#]{${this.value.length}}`;
   render(){
-    const randomId = Math.random().toString(16).substring(2);
-    console.log(countries);
+
     return (
       <div>
         <input
-          list={randomId}
-          defaultValue={"dupa"}
+          id="inputValue"
+          list="countries"
+          name='inputValue'
+          value={this.state.inputValue}
+          onChange={this.handleInputChange.bind(this)}
+          onBlur={this.handleOnBlur.bind(this)}
+          onFocus={this.handleOnFocus.bind(this)}
         ></input>
-        <datalist
-          id={randomId}
-        >{
-
-        }
+        <datalist id='countries'>
+          {
+            this.state.countries.map((item, index)=>{
+              return <option value={item.name} key={index}>({item.code})</option>
+            })
+          }
         </datalist>
+        <p>{this.state.inputValue}</p>
       </div>
     )
   }
